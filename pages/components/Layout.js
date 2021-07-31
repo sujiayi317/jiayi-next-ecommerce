75fa7c739bfe2abc1a9c46ad012a4fe6d677ca07
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-07-30 20:49:56 -0400

    Add style to Layout

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,21 +1,28 @@
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
 import Head from 'next/head'
 import React from 'react'
+import useStyles from '../../utils/styles'
 
 export default function Layout({children}) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Jiayi-ecommerce</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>Jiayi-store</Typography>
+          <Typography>Jiayi-Store</Typography>
         </Toolbar>
       </AppBar>
-      <Container>
+      <Container className={classes.main}>
         {children}
       </Container>
+      <footer className={classes.footer}>
+        <Typography>
+          All rights reserved. Next Jiayi-Ecommerce Store
+        </Typography>
+      </footer>
     </div>
   )
 }
