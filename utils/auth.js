571a05842d5ac6570d5d24db199ded491269c17c
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-08-01 09:15:48 -0400

    Build Login API 3. use jsonwebtoken to sign token 4. implement login api

---
+++ utils/auth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+const signToken = (user) => {
+  // jwt.sign(payload, secretOrPrivateKey, [options, callback])
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '30d',
+    }
+  );
+};
+
+export { signToken };
