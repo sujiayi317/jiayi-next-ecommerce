4b51b51192ca9282757b95969a6a6b070f4c20f2
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-07-31 23:26:14 -0400

    Create Sample Users     1. create user model     2. add sample user in seed api

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -2,12 +2,15 @@ import nc from 'next-connect';
 import Product from '../../models/Product';
 import db from '../../utils/db';
 import data from '../../utils/data';
+import User from '../../models/User';
 
 const handler = nc();
 
 // seed data to the db
 handler.get(async (req, res) => {
   await db.connect();
+  await User.deleteMany();
+  await User.insertMany(data.users);
   await Product.deleteMany();
   await Product.insertMany(data.products);
   await db.disconnect();
